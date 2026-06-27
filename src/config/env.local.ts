6a4f8756b8437aa7559639da12d1870dcd1e1447
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-06-27 22:48:30 +0300

    Salt round hash

--- src/config/env.local.ts
+++ src/config/env.local.ts
@@ -2,5 +2,6 @@ import { config } from "dotenv";
 config();
 
 export const env = {
-    mongoUri: process.env.MONGO_URI 
+    mongoUri: process.env.MONGO_URI,
+    SALT_ROUND: process.env.SALT_ROUND,
 };
