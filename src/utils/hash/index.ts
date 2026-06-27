93627316abc240d47757e3d48a4228024526857a
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-06-27 22:49:03 +0300

    modified hashing

--- src/utils/hash/index.ts
+++ src/utils/hash/index.ts
@@ -5,8 +5,8 @@ import { env } from "../../config/env.local";
 const saltRounde = parseInt(env.SALT_ROUND!);
 
 export const hashValue = async (value: string) => {
-    await bcrypt.hash(value, saltRounde);
+    return (await bcrypt.hash(value, saltRounde)).toString();
 }
 export const compareValue = async ({ value, hash }: { value: string, hash: string }) => {
-    await bcrypt.compare(value, hash);
+    return await bcrypt.compare(value, hash);
 }
