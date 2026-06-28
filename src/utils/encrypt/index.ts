7055ffc232dd7123dca472ab5f90ddca4780e272
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-06-28 21:54:41 +0300

    edit decrypto method

---
+++ src/utils/encrypt/index.ts
@@ -0,0 +1,23 @@
+﻿import CryptoJS from "crypto-js";
+import { env } from "../../config/env.local";
+
+const keyCrypto = env.ENCRYPT_KEY!;
+
+const encryptValue = (data: string): string => {
+    return CryptoJS.AES.encrypt(data, keyCrypto).toString();
+};
+
+const decryptValue = (data: string): string => {
+    if (!data) {
+        return "";
+    }
+    try {
+        const decryptoData = CryptoJS.AES.decrypt(data, keyCrypto).toString(CryptoJS.enc.Utf8);
+        return decryptoData;
+    } catch (error) {
+        console.error("decryptValue: Decryption failed", error);
+        return "";
+    }
+};
+
+export { encryptValue, decryptValue };
