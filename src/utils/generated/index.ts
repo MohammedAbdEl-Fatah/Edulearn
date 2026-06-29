45cf87f4b76d000bd38b1dec8ee1d457d4252e60
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-06-30 01:25:34 +0300

    done create method generate token

--- src/utils/generated/index.ts
+++ src/utils/generated/index.ts
@@ -1,3 +1,8 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { RoleUSER, TokenType } from "../enum";
+import { env } from "../../config/env.local";
+import ms from "ms";
+
 // generate otp
 export const generateOtp = () => {
     return Math.floor(100000 + Math.random() * 900000).toString();// like 000000 6digit
@@ -5,3 +10,28 @@ export const generateOtp = () => {
 export const generateOtpExpire = (time: number = 5) => {
     return new Date(Date.now() + time * 60 * 1000);//like time =10; => 10 minutes default 5 minites
 }
+// generate Token: access and refresh token for student and teacher
+const secretUserStudentAccessToken = env.SECRET_TOKEN_ACCESS_STUDENT!;
+const secretUserTeacherAccessToken = env.SECRET_TOKEN_ACCESS_TEACHER!;
+const secretUserStudentRefreshToken = env.SECRET_TOKEN_REFRESH_STUDENT!;
+const secretUserTeacherRefreshToken = env.SECRET_TOKEN_REFRESH_TEACHER!;
+
+
+export const generatedToken = ({ data, roleSecret, time, tokenType }:
+    { data: any, roleSecret: string, time: number | ms.StringValue, tokenType: string }) => {
+    if (roleSecret === RoleUSER.STUDENT) {
+        if (tokenType === TokenType.ACCESS) {
+            return jwt.sign(data, secretUserStudentAccessToken, { expiresIn: time = '30m' });
+        }
+        return jwt.sign(data, secretUserStudentRefreshToken, { expiresIn: time = '7d' });
+    }
+
+    if (roleSecret === RoleUSER.TEACHER) {
+        if (tokenType === TokenType.ACCESS) {
+            return jwt.sign(data, secretUserTeacherAccessToken, { expiresIn: time = '30m' });
+        }
+        return jwt.sign(data, secretUserTeacherRefreshToken, { expiresIn: time = '7d' });
+    }
+
+    throw new Error('Invalid role');
+};
