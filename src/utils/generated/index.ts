e412097054f4f1454fce4a5470058e7d889f0f52
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-07-04 22:42:50 +0300

    clean code Token  Secret

--- src/utils/generated/index.ts
+++ src/utils/generated/index.ts
@@ -11,26 +11,28 @@ export const generateOtpExpire = (time: number = 5) => {
     return new Date(Date.now() + time * 60 * 1000);//like time =10; => 10 minutes default 5 minites
 }
 // generate Token: access and refresh token for student and teacher
-const secretUserStudentAccessToken = env.SECRET_TOKEN_ACCESS_STUDENT!;
-const secretUserTeacherAccessToken = env.SECRET_TOKEN_ACCESS_TEACHER!;
-const secretUserStudentRefreshToken = env.SECRET_TOKEN_REFRESH_STUDENT!;
-const secretUserTeacherRefreshToken = env.SECRET_TOKEN_REFRESH_TEACHER!;
+export const TokenSecret = {
+    secretUserStudentAccessToken: env.SECRET_TOKEN_ACCESS_STUDENT!,
+    secretUserTeacherAccessToken: env.SECRET_TOKEN_ACCESS_TEACHER!,
+    secretUserStudentRefreshToken: env.SECRET_TOKEN_REFRESH_STUDENT!,
+    secretUserTeacherRefreshToken: env.SECRET_TOKEN_REFRESH_TEACHER!,
+}
 
 
 export const generatedToken = ({ data, roleSecret, time, tokenType }:
     { data: any, roleSecret: string, time: number | ms.StringValue, tokenType: string }) => {
     if (roleSecret === RoleUSER.STUDENT) {
         if (tokenType === TokenType.ACCESS) {
-            return jwt.sign(data, secretUserStudentAccessToken, { expiresIn: time = '30m' });
+            return jwt.sign(data, TokenSecret.secretUserStudentAccessToken, { expiresIn: time = '30m' });
         }
-        return jwt.sign(data, secretUserStudentRefreshToken, { expiresIn: time = '7d' });
+        return jwt.sign(data, TokenSecret.secretUserStudentRefreshToken, { expiresIn: time = '7d' });
     }
 
     if (roleSecret === RoleUSER.TEACHER) {
         if (tokenType === TokenType.ACCESS) {
-            return jwt.sign(data, secretUserTeacherAccessToken, { expiresIn: time = '30m' });
+            return jwt.sign(data, TokenSecret.secretUserTeacherAccessToken, { expiresIn: time = '30m' });
         }
-        return jwt.sign(data, secretUserTeacherRefreshToken, { expiresIn: time = '7d' });
+        return jwt.sign(data, TokenSecret.secretUserTeacherRefreshToken, { expiresIn: time = '7d' });
     }
 
     throw new Error('Invalid role');
