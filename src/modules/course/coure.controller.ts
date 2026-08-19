51e29edb03d745190b2885aea9217a3b0bbae2bc
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-08-19 17:38:50 +0300

    feat: implement course module structure and user authentication schemas with updated TS configuration

---
+++ src/modules/course/coure.controller.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+
+const courseRouter = Router();
+
+courseRouter.post("")
--- src/utils/interface/index.ts
+++ src/utils/interface/index.ts
@@ -1,6 +1,6 @@
 export interface IUser {
     readonly id: string;
-    fristName: string;
+    firstName: string;
     lastName: string;
     email: string;
     password: string;
