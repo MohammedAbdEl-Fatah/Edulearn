98de2dd1f7f65314097bb1a8782b017b10105488
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-08-29 11:51:10 +0300

    interface for course,session,review,video and pdf.

--- src/utils/interface/index.ts
+++ src/utils/interface/index.ts
@@ -22,3 +22,55 @@ export interface IToken {
     expires: Date;
     isRevoked: boolean;
 }
+export interface ICourse {
+    readonly id: string;
+    instructorId: string;
+    title: string;
+    description: string;
+    //price and discount
+    price: number;
+    discount: number;
+    sessions: ISession[];
+    // duration: number;
+    category: string;
+    reviews: IReview[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+export interface IReview {
+    readonly id: string;
+    userId: string;
+    courseId: string;
+    rating: number;
+    comment: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ISession {
+    readonly id: string;
+    courseId: string;
+    title: string;
+    videos:IVideo[];
+    pdfs:IPdf[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IVideo {
+    readonly id: string;
+    sessionId: string;
+    title: string;
+    url: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IPdf {
+    readonly id: string;
+    sessionId: string;
+    title: string;
+    url: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
